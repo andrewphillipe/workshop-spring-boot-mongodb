861593feceaa5a91f501eb4c0629981e62b0dc79
Andrew Phillipe <andrewphillipe@hotmail.com>
2022-07-04 16:57:53 -0300

    refarecing post with users

--- src/main/java/br/com/company/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/company/workshopmongo/config/Instantiation.java
@@ -38,10 +38,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(us1, us2, us3));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "#partiu", "Vou viajar bla bla bla 1", new AuthorDTO(us3));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "#partiu", "Vou viajar bla bla bla 1", new AuthorDTO(us1));
 		Post post2 = new Post(null, sdf.parse("22/04/2019"), "#partiu", "Vou viajar bla bla bla 2", new AuthorDTO(us2));
 		Post post3 = new Post(null, sdf.parse("23/05/2020"), "#partiu", "Vou viajar bla bla bla 3", new AuthorDTO(us1));
 
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
+		
+		us1.getPosts().addAll(Arrays.asList(post1,post3));
+		
+		userRepository.save(us1);
 	}
 }
