86b757488857ef1374289a55c3e742eed69357d4
Andrew Phillipe <andrewphillipe@hotmail.com>
2022-07-04 15:37:57 -0300

    insertion user with post verb

--- src/main/java/br/com/company/workshopmongo/services/UserService.java
+++ src/main/java/br/com/company/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.company.workshopmongo.domain.User;
+import br.com.company.workshopmongo.dto.UserDTO;
 import br.com.company.workshopmongo.repositories.UserRepository;
 import br.com.company.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
