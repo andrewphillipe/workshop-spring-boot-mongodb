a66e218d711e3eb21c08f4e147eacffea2cdbd3b
Andrew Phillipe <andrewphillipe@hotmail.com>
2022-07-04 17:08:11 -0300

    endpoint to return posts by id

---
+++ src/main/java/br/com/company/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.company.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.company.workshopmongo.domain.Post;
+import br.com.company.workshopmongo.repositories.PostRepository;
+import br.com.company.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+
+}
